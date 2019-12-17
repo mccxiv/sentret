09ada91ac3b83392144469f5c3b16cde49ae55e3
Andrea Stella <adr.stella@gmail.com>
2019-12-17 17:15:10 +0800

    Add some more features

--- src/sentret.ts
+++ src/sentret.ts
@@ -1,13 +1,47 @@
-const EVENT_CLASS_PREFIX = 'se-'
-const EVENT_DATA_ATTRIBUTE = 'se-properties'
+const defaults: SentretOptions = {
+  log: true,
+  parseAsJson: false,
+  classPrefix: 'se-',
+  propertiesAttribute: 'properties',
+}
 
-console.info('Sentret loaded')
-
-export function Sentret(): SentretInstance {
-  console.info('Sentret instance created')
+export function Sentret(options: SentretOptions): SentretInstance {
   const callbacks: SentretClickCallback[] = []
+  const listenerInstance = (event: MouseEvent) => globalClickListener(event)
+  const opts = {...defaults, ...options}
+  let listening = false
 
-  document.addEventListener('click', globalClickListener)
+  return {
+    on (eventType, callback: SentretClickCallback) {
+      if (eventType === 'event') {
+        if (!listening) initialize()
+        callbacks.push(callback)
+      }
+      else throw (Error(`Invalid event. Sentret does not emit "${eventType}"`))
+    },
+    destroy () {
+      document.removeEventListener('click', listenerInstance)
+    }
+  }
+
+  function log (message: string, data?: object) {
+    if (opts.log) {
+      console.log(
+        `%c[Sentret]:`,
+        'color: #8c6b5e; font-weight: bold',
+        `${message}`,
+        data || ''
+      )
+    }
+  }
+
+  function initialize () {
+    document.addEventListener('click', listenerInstance)
+    listening = true
+
+    log('Sentret is duty')
+    log('Turn this logging off for production with {log: false}')
+  }
 
   function globalClickListener (event: MouseEvent) {
     let currentElement = event.target as HTMLElement | null
@@ -15,38 +49,25 @@ export function Sentret(): SentretInstance {
     let eventData: any
 
     while (!eventName && currentElement) {
-      if (currentElement.matches(`[class^=${EVENT_CLASS_PREFIX}]`)) {
+      if (currentElement.matches(`[class^=${opts.classPrefix}]`)) {
         const eventClass = Array.from(currentElement.classList)
-          .find(className => className.startsWith(EVENT_CLASS_PREFIX))
+          .find(className => className.startsWith(opts.classPrefix))
         if (eventClass) {
-          eventName = eventClass.replace(EVENT_CLASS_PREFIX, '')
-          eventData = currentElement.dataset[EVENT_DATA_ATTRIBUTE]
+          eventName = eventClass.replace(opts.classPrefix, '')
+          eventData = currentElement.dataset[opts.propertiesAttribute]
         }
       } else currentElement = currentElement.parentElement
     }
 
     if (eventName) {
-      console.group('Sentret Event')
-      console.info(`Event name: ${eventName}`)
-      if (eventData) {
-        console.info(`Event data:`, eventData)
-        console.log('type of data', typeof eventData)
-      }
-      console.groupEnd()
+      log('Click event captured')
+      log(`• Event: ${eventName}`)
+      if (eventData) log('• Properties:', JSON.parse(eventData))
 
       const name = eventName // Reassign due to Typescript null check
-      callbacks.forEach(fn => fn(name, eventData))
-    }
-
-
-  }
-
-  return {
-    onClick (callback: SentretClickCallback) {
-      callbacks.push(callback)
-    },
-    destroy () {
-      document.removeEventListener('click', globalClickListener)
+      callbacks.forEach(fn => {
+        fn(name, eventData)
+      })
     }
   }
 }
@@ -54,6 +75,13 @@ export function Sentret(): SentretInstance {
 type SentretClickCallback = (eventName: string, data?: any) => any
 
 interface SentretInstance {
-  onClick: (cb: SentretClickCallback) => void
+  on: (eventType: 'event', cb: SentretClickCallback) => void
   destroy: () => void
+}
+
+interface SentretOptions {
+  log: boolean
+  parseAsJson: boolean
+  classPrefix: string
+  propertiesAttribute: string
 }
